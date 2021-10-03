209bb6c7b295cea33a7ec16e1286985aa8103714
SushiWaUmai <eugene.bb1@gmail.com>
2021-10-03 21:02:51 +0200

    :zap: add spdlog

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 #include "SDL.h"
+#include "spdlog/spdlog.h"
 
-#define LOG(x) std::cout << x << std::endl
+#define LOG_TRACE(...) spdlog::trace(__VA_ARGS__)
+#define LOG_INFO(...) spdlog::info(__VA_ARGS__)
+#define LOG_DEBUG(...) spdlog::debug(__VA_ARGS__)
+#define LOG_WARNING(...) spdlog::warn(__VA_ARGS__)
+#define LOG_ERROR(...) spdlog::error(__VA_ARGS__)
+#define LOG_CRITICAL(...) spdlog::critical(__VA_ARGS__)
+
+#ifdef _WIN32
+#define DEBUG_BREAK() __debugbreak()
+#else
+#include <signal.h>
+#define raise(SIGTRAP)
+#endif
+
+#define LOG_ASSERT(check, msg)	{ if(!check) { LOG_ERROR("Assertion Failed: {0}", msg); } }
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
@@ -11,21 +26,22 @@ int main(int argc, char* args[]) {
 	(void)args;
 
 	if (SDL_Init(SDL_INIT_EVERYTHING)) {
-		LOG("SDL could not be initialized");
+		LOG_ERROR("SDL could not be initialized: {0}", SDL_GetError());
 		SDL_Quit();
 		return EXIT_FAILURE;
 	}
 
 	SDL_Window* window = SDL_CreateWindow("Minesweeper", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 	if (!window) {
-		LOG("Failed to create SDL Window");
+		LOG_ERROR("Failed to create SDL Window: {0}", SDL_GetError());
 		SDL_Quit();
 		return EXIT_FAILURE;
 	}
 
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 	if (!renderer) {
-		LOG("Failed to create SDL renderer");
+		LOG_ERROR("Failed to create SDL renderer: {0}", SDL_GetError());
+		SDL_DestroyWindow(window);
 		SDL_Quit();
 		return EXIT_FAILURE;
 	}
@@ -38,6 +54,7 @@ int main(int argc, char* args[]) {
 			break;
 	}
 
+	SDL_DestroyWindow(window);
 	SDL_Quit();
 
 	return EXIT_SUCCESS;
